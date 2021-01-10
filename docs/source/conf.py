531fcd2e7df543605c05e9950892cdbf5daf559f
Jinki Jung <your.jinki.jung@gmail.com>
2021-01-10 21:16:30 +0100

    dirty export of pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,15 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'MOAD Annotation Tool'
-copyright = '2020, Jinki Jung'
+project = 'MOAD annotation tool'
+copyright = '2021, Digital Maritime Consultancy ApS'
 author = 'Jinki Jung'
 
 # The full version, including alpha/beta/rc tags
@@ -54,7 +54,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
